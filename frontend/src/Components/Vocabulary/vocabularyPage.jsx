429464561f00b0a8aa43f143f907241d78a83ca6
Bin-08-01 <minhtri192035@gmail.com>
2022-09-21 23:12:30 +0700

    🚀 Edit Style

--- frontend/src/Components/Vocabulary/vocabularyPage.jsx
+++ frontend/src/Components/Vocabulary/vocabularyPage.jsx
@@ -1,4 +1,4 @@
-
+import "./vocabularyPage.css";
 import {Link} from "react-router-dom";
 
 
@@ -6,12 +6,24 @@ const VocabularyPage = () => {
 
     return (
         <div className={"vocabulary-page-container"}>
-            Language:
-            <div>
-                <Link to={'/view?language=english'}>English</Link>
-            </div>
-            <div>
-                <Link to={'/view?language=france'}>France</Link>
+            <h3 className={"choose-language-title"}>Language:</h3>
+            <div className={"flashcard-container"}>
+                <Link to={'/view?language=english'} className={"language-flashcard"}>
+                    <div className={"image-language"}>
+                        <img src="https://haycafe.vn/wp-content/uploads/2021/11/Anh-avatar-dep-chat-lam-hinh-dai-dien.jpg" alt=""/>
+                    </div>
+                    <div className={"name-language"}>
+                        <p>English</p>
+                    </div>
+                </Link>
+                <Link to={'/view?language=france'} className={"language-flashcard"}>
+                    <div className={"image-language"}>
+                        <img src="https://haycafe.vn/wp-content/uploads/2021/11/Anh-avatar-dep-chat-lam-hinh-dai-dien.jpg" alt=""/>
+                    </div>
+                    <div className={"name-language"}>
+                        <p>France</p>
+                    </div>
+                </Link>
             </div>
 
         </div>
